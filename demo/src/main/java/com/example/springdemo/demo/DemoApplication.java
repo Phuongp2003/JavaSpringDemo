f90275afb8cf855f7a5414b5519c4d3e439e29f2
Phuong <phambuinamphuong@gmail.com>
2023-12-28 02:49:24 +0700

    Fix /home, update css /home, improve username show

--- demo/src/main/java/com/example/springdemo/demo/DemoApplication.java
+++ demo/src/main/java/com/example/springdemo/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.springdemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
@@ -18,10 +20,6 @@ public class DemoApplication {
 	@GetMapping({ "/", "/home" })
 	public String home(HttpSession session, Model model) {
 		String username = (String) session.getAttribute("username");
-		if (username == null || username.isEmpty()) {
-			username = "None";
-			session.setAttribute("username", username);
-		}
 		model.addAttribute("username", username);
 		return "home";
 	}
--- demo/src/main/java/com/example/springdemo/demo/LoginForm.java
+++ demo/src/main/java/com/example/springdemo/demo/LoginForm.java
@@ -1,5 +1,6 @@
 package com.example.springdemo.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+
 @Controller
 public class LoginForm {
+    private static final Logger logger = LoggerFactory.getLogger(LoginForm.class);
 
     @GetMapping("/login")
     public String login() {
@@ -17,28 +23,39 @@ public class LoginForm {
 
     @PostMapping("/login")
     public String handleLogin(@RequestParam String username, @RequestParam String password,
-            RedirectAttributes redirectAttributes) {
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
-        redirectAttributes.addFlashAttribute("username", username);
+            HttpSession session) {
+        session.setAttribute("username", username);
         return "redirect:/loginSuccess";
     }
 
     @GetMapping("/loginSuccess")
-    public String loginSuccess(Model model) {
-        // The username will be available in the model
-        String username = (String) model.getAttribute("username");
-        System.out.println("Username: " + username);
+    public String loginSuccess(Model model, HttpSession session) {
+        // The username will be available in the session
+        String username = (String) session.getAttribute("username");
 
-        // If the username is null or empty, set it to "None"
         if (username == null || username.isEmpty()) {
-            username = "None";
+            logger.info("No username present, redirecting to login page.");
+            return "redirect:/login";
         }
 
-        // Add the username to the model
+        logger.info("Username {} login success!", username);
         model.addAttribute("username", username);
 
-        return "home";
+        return "redirect:/home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session, RedirectAttributes redirectAttributes) {
+        String username = (String) session.getAttribute("username");
+        if (username == null || username.isEmpty()) {
+            logger.info("No username present, redirecting to login page.");
+            return "redirect:/home";
+        }
+
+        logger.info("Username {} logout success!", username);
+        redirectAttributes.addFlashAttribute("logout", true);
+        session.removeAttribute("username");
+        return "redirect:/home";
     }
 
 }
